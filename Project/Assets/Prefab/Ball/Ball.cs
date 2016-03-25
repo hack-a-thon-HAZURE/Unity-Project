114f491846c7dabd4f32e7f2adf8b1b5a53fed63
AyumiYasui <anzu.ya0627@gmail.com>
2016-03-25 15:55:29 +0900

    ボールが最初ランダムな角度で飛んでいく

--- Project/Assets/Prefab/Ball/Ball.cs
+++ Project/Assets/Prefab/Ball/Ball.cs
@@ -25,9 +25,18 @@ public class Ball : MonoBehaviour
     void Start () 
     {
         // ボールが飛ぶ方向(左右)をランダムで決めている
-        int Dir = (Random.Range(0, 2) == 0 ? -1 : 1);
+        int LR = (Random.Range(0, 2) == 0 ? -1 : 1);
+        float RotZ = Random.Range(Mathf.PI/4.0f, -Mathf.PI /4.0f);
 
-        this.GetComponent<Rigidbody>().AddForce(Dir * transform.right * BallSpeed, ForceMode.VelocityChange);
+        // ベクトルの生成
+        Vector3 DirNor = transform.right;
+        DirNor *= LR;
+
+        Vector3 Dir = new Vector3();
+        Dir.x =  Mathf.Cos(RotZ) * DirNor.x + Mathf.Sin(RotZ) * DirNor.y;
+        Dir.y = -Mathf.Sin(RotZ) * DirNor.x + Mathf.Cos(RotZ) * DirNor.y;
+
+        this.GetComponent<Rigidbody>().AddForce(Dir * BallSpeed, ForceMode.VelocityChange);
     }
 
     /// <summary>
