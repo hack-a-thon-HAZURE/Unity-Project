c9523a404535b866363a6237781e6f2581e1f9fa
AyumiYasui <anzu.ya0627@gmail.com>
2016-03-25 12:24:37 +0900

    PowerMate対応

--- Project/Assets/InputManager.cs
+++ Project/Assets/InputManager.cs
@@ -13,15 +13,24 @@ public class InputManager : MonoBehaviour
     public GameObject PlayerTwo;
 
     public float MoveSpeed;
+    public float MaxMoveSpeed;
 
     public float MaxHeightRange;
+
+
+    private Rigidbody PlayerOneRigid;
+    private Rigidbody PlayerTwoRigid;
+
+    private float PlayerOneVirtualKey;
+    private float PlayerTwoVirtualKey;
 
     /// <summary>
     /// Awake is called when the script instance is being loaded.
     /// </summary>
     void Awake()
     {
-
+        PlayerOneVirtualKey = 0;
+        PlayerTwoVirtualKey = 0;
     }
 
     /// <summary>
@@ -29,7 +38,8 @@ public class InputManager : MonoBehaviour
     /// </summary>
     void Start () 
     {
-
+        PlayerOneRigid = PlayerOne.GetComponent<Rigidbody>();
+        PlayerTwoRigid = PlayerTwo.GetComponent<Rigidbody>();
     }
 
     /// <summary>
@@ -38,38 +48,64 @@ public class InputManager : MonoBehaviour
     void Update () 
     {
         // 1P操作
-        if(Input.GetKey(KeyCode.W))
         {
             Vector3 Pos = PlayerOne.transform.position;
-            Pos.y += MoveSpeed;
+            if (Input.GetKey(KeyCode.W))
+            {
+                PlayerOneVirtualKey += MoveSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                PlayerOneVirtualKey -= MoveSpeed;
+            }
+
+            // Mouse Xにアサイン
+            {
+                float move_1p = Input.GetAxis("Mouse X");
+                PlayerOneVirtualKey -= move_1p;
+            }
+
+            PlayerOneVirtualKey += (0 - PlayerOneVirtualKey) * 0.5f;
+            float add = PlayerOneVirtualKey;
+
+            if (add < -MaxMoveSpeed) add = -MaxMoveSpeed;
+            if (add >  MaxMoveSpeed) add =  MaxMoveSpeed;
+
+            Pos.y += add;
             Pos.y = Mathf.Clamp(Pos.y, -MaxHeightRange, MaxHeightRange);
-            PlayerOne.transform.position = Pos;
+            PlayerOneRigid.MovePosition(Pos);
         }
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            Vector3 Pos = PlayerOne.transform.position;
-            Pos.y -= MoveSpeed;
-            Pos.y = Mathf.Clamp(Pos.y, -MaxHeightRange, MaxHeightRange);
-            PlayerOne.transform.position = Pos;
-        }
-
-        // 2P操作
-        if (Input.GetKey(KeyCode.UpArrow))
+        // 1P操作
         {
             Vector3 Pos = PlayerTwo.transform.position;
-            Pos.y += MoveSpeed;
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                PlayerTwoVirtualKey += MoveSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                PlayerTwoVirtualKey -= MoveSpeed;
+            }
+
+            {
+                float move_1p = Input.GetAxis("Mouse Y");
+                PlayerTwoVirtualKey -= move_1p;
+            }
+
+            PlayerTwoVirtualKey += (0 - PlayerTwoVirtualKey) * 0.5f;
+            float add = PlayerTwoVirtualKey;
+
+            if (add < -MaxMoveSpeed) add = -MaxMoveSpeed;
+            if (add > MaxMoveSpeed) add = MaxMoveSpeed;
+
+            Pos.y += add;
             Pos.y = Mathf.Clamp(Pos.y, -MaxHeightRange, MaxHeightRange);
-            PlayerTwo.transform.position = Pos;
+            PlayerTwoRigid.MovePosition(Pos);
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            Vector3 Pos = PlayerTwo.transform.position;
-            Pos.y -= MoveSpeed;
-            Pos.y = Mathf.Clamp(Pos.y, -MaxHeightRange, MaxHeightRange);
-            PlayerTwo.transform.position = Pos;
-        }
     }
 }
 
