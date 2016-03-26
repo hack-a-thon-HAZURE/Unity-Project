1eafbfc1bb62bdd299200251590ee69df6eb711e
AyumiYasui <anzu.ya0627@gmail.com>
2016-03-26 12:05:21 +0900

    ボールを捕まえて加速するようにした

--- Project/Assets/Prefab/Ball/Ball.cs
+++ Project/Assets/Prefab/Ball/Ball.cs
@@ -18,6 +18,9 @@ public class Ball : MonoBehaviour
     private Vector2 vec;    // ベクトル
     private int layerMask;  // レイヤーマスク
 
+    // プロパティ
+    public Vector2 Vec { get { return vec; } set { vec = value; } }
+
     /// <summary>
     /// Awake is called when the script instance is being loaded.
     /// </summary>
@@ -51,7 +54,8 @@ public class Ball : MonoBehaviour
     {
         // あたり判定の結果によって、ボールの動きが変化する
         var info = new RaycastHit();
-        bool is_will_hit = Physics.Raycast(transform.position, vec.normalized, out info, vec.magnitude * Time.deltaTime , layerMask);
+        bool is_will_hit =
+            Physics.Raycast(transform.position, vec.normalized, out info, vec.magnitude * Time.deltaTime , layerMask);
         if (!is_will_hit) { Move(); }
         else { Move(info); }
         Debug.DrawRay(transform.position, vec*Time.deltaTime, Color.red);
@@ -62,7 +66,7 @@ public class Ball : MonoBehaviour
     /// </summary>
     private void Move(RaycastHit info)
     {
-        Debug.Log("Hit Ball : " + info.collider.name);
+        //Debug.Log("Hit Ball : " + info.collider.name);
         Vector2 pos_old = transform.position;
         Vector2 info_point = info.point;
 
@@ -82,9 +86,13 @@ public class Ball : MonoBehaviour
 
         // 移動結果後の位置
         transform.position = pos_old + ((vec_nor * length_before_hit) + (vec_ref_nor * length_after_hit)) * Time.deltaTime;
+        rigid.MovePosition(transform.position);
 
         // 最終的なベクトル
         vec = vec_ref_nor * vec_length;
+
+        // 加速
+        AddSpeedIfHit();
     }
 
     /// <summary>
@@ -96,6 +104,18 @@ public class Ball : MonoBehaviour
         Vector2 pos = transform.position;
         pos += vec * Time.deltaTime;
         transform.position = pos;
+    }
+
+
+
+    /// <summary>
+    /// 何かに当たったら加速
+    /// </summary>
+    private void AddSpeedIfHit()
+    {
+        var vec_nor = vec.normalized;
+        var speed = vec.magnitude;
+        vec = vec_nor * (speed + AddBallSpeed);
     }
 
 }
---
+++ Project/Assets/Script/game/ForceBallSpeed.cs
@@ -0,0 +1,117 @@
+﻿/**************************************************************************************************
+
+ @File   : [ ForceBallSpeed ] 
+ @Auther : Ayumi Yasui
+
+**************************************************************************************************/
+using UnityEngine;
+using System.Collections;
+
+public class ForceBallSpeed : MonoBehaviour {
+
+    /// メンバ変数
+    public float HoldTime;  // 捕まえておく時間
+    public float AddSpeed;  // 追加する速さ
+
+    private Ball  ball;      // ボール
+    private bool  is_catch;  // ボールを捕まえた
+    private float hold_time_count;  // 捕まえておくまでの時間
+
+    /// プロパティ
+    public bool IsCatch { get { return is_catch; } }
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        ball            = null;
+        is_catch = false;
+        hold_time_count = HoldTime;
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        if (!IsCatch) return;
+        hold_time_count -= Time.deltaTime;
+        if (hold_time_count < 0.0f) EndCatch();
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void OnTriggerEnter(Collider coll)
+    {
+        if (ball) Debug.Log("Catch Ready ?");
+        if (IsCatch) return;
+
+        // ボールスクリプトの取得
+        ball = coll.GetComponent<Ball>();
+        if (ball) Debug.Log("Catch Ready");
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void OnTriggerExit(Collider coll)
+    {
+        if (IsCatch) return;
+
+        // ボールスクリプトの取得
+        if(ball) if (ball.gameObject == coll.gameObject) ball = null;
+        if (!ball) Debug.Log("Catch End");
+    }
+
+    /// <summary>
+    /// キャッチ開始
+    /// </summary>
+    public void StartCatch()
+    {
+        Debug.Log("Try Catch");
+
+        if (IsCatch) return;
+        if (!ball) return;
+        enabled = true;
+
+        is_catch = true;
+        hold_time_count = HoldTime;
+
+        ball.enabled = false;
+        ball.gameObject.transform.parent = transform;
+
+        // 速さの追加
+        var vec   = ball.Vec.normalized;
+        var speed = ball.Vec.magnitude;
+
+        speed += AddSpeed;
+        vec   *= -speed;
+
+        ball.Vec = vec;
+
+        Debug.Log("Catch");
+    }
+
+    /// <summary>
+    /// キャッチ終了
+    /// </summary>
+    private void EndCatch()
+    {
+        if (!IsCatch) return;
+        //enabled = false;
+
+        is_catch = false;
+        hold_time_count = HoldTime;
+
+        ball.enabled = true;
+        ball.gameObject.transform.parent = null;
+        ball = null;
+    }
+}
+//===============================================================================================//
+//                                                                                               //
+//                                          @End of File                                         //
+//                                                                                               //
+//===============================================================================================//
