16d4ef9d7fa4d541bcf6f370f7bf79eb14ad4e9a
hureikuru <hureikuru@yahoo.co.jp>
2016-03-26 10:29:55 +0900

    エフェクト系追加。衝撃波プレハブ

---
+++ Project/Assets/Effects/Script/BurstEffect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BurstEffect : MonoBehaviour {
+
+    public float LifeTime = 1.0f;
+    private float LifeTimeCnt =0.0f;
+    public float ScalingValue = 20.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 Scl = transform.localScale;
+
+        Scl.x += ScalingValue * Time.deltaTime;
+        Scl.y += ScalingValue * Time.deltaTime;
+
+        transform.localScale = Scl;
+
+        LifeTimeCnt += Time.deltaTime;
+
+        if (LifeTimeCnt >= LifeTime)
+        {
+            Destroy(gameObject);
+        }
+
+
+	}
+}
